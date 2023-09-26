633d9a7847dd8746ad4406d4370b78195f93df9f
Sameer Sagar <60567209+sameer2002@users.noreply.github.com>
2023-09-26 19:36:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,11 +11,35 @@ function convertToRoman(num) {
 
   //your code here
 
+	 let romanNumeral = "";
+
+  for (let i = 0; i < obj.length; i++) {
+    while (num >= obj[i][1]) {
+      romanNumeral += obj[i][0];
+      num -= obj[i][1];
+    }
+  }
+
+  return romanNumeral;
+
 }
+console.log(btn)
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
+const inputz=document.getElementById("btn");
+inputz.addEventListener("click",function(){
+    const gotInput=input.value
+     console.log(gotInput)
+     const output1 = convertToRoman(gotInput); // Output will be "XIV"
 
+console.log(output1);
+let container=document.getElementsByClassName("container");
+
+let roman=document.getElementById("roman")
+roman.innerText=output1
+
+})
 
 
 
